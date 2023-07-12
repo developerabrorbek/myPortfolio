07ffb674de2865dacfa6e4ff28a89b47907ff78f
Abrorbek abdulkhamidov <abdulkhamidovabrorbek@gmail.com>
2023-07-12 15:06:01 +0500

    Routes enabled and structure built

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Skills from "./pages/Skills";
+import Works from "./pages/Works";
+import Contact from "./pages/Contact";
+import Error from "./pages/Error";
 
 const App = () => {
   return (
-    <div>
-      <h1>salom</h1>
-    </div>
+    <>
+      <Routes>
+        <Route element={<Home />} path="/" />
+        <Route element={<Skills />} path="/skills" />
+        <Route element={<Works />} path="/works" />
+        <Route element={<Contact />} path="/contact" />
+        <Route element={<Error />} path="*" />
+      </Routes>
+    </>
   );
 };
 
---
+++ src/pages/Error/index.jsx
@@ -0,0 +1,10 @@
+
+const Error = () => {
+  return (
+    <div>
+      <h1>Error page</h1>
+    </div>
+  );
+};
+
+export default Error;

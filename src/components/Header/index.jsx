f75ec4713e5bd51ba08053b7aa73c110d08bc0fa
Abrorbek abdulkhamidov <abdulkhamidovabrorbek@gmail.com>
2023-08-23 15:53:00 +0500

    updated

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,16 +5,16 @@ const Header = () => {
       <div className="container mx-auto max-w-7xl">
         <nav>
           <ul className="list-items flex items-center gap-x-8 justify-end">
-            <li className="list-item text-white text-[18px] hover:underline">
+            <li className="list-item text-white text-[22px] hover:underline">
               <NavLink to="/">Home</NavLink>
             </li>
-            <li className="list-item text-white text-[18px] hover:underline">
+            <li className="list-item text-white text-[22px] hover:underline">
               <NavLink to="/skills">Skills</NavLink>
             </li>
-            <li className="list-item text-white text-[18px] hover:underline">
+            <li className="list-item text-white text-[22px] hover:underline">
               <NavLink to="/works">Works</NavLink>
             </li>
-            <li className="list-item text-white text-[18px] hover:underline">
+            <li className="list-item text-white text-[22px] hover:underline">
               <NavLink to="/contact">Contact</NavLink>
             </li>
           </ul>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,17 +9,23 @@ const Home = () => {
       <main className="main flex h-3/4 slide-top">
         <div className="main container mx-auto">
           <div className="main-body h-full flex flex-col items-center justify-center gap-y-6">
-            <h1 className="main-body__title text-[42px] text-white">
+            <h1 className="main-body__title text-[48px] text-white">
               Hey I am Abrorbek Abdulxamidov
             </h1>
-            <p className="main-body__subtitle text-[28px] text-[#d9cac5]">
-              I am a FrontEnd developer
+            <p className="main-body__subtitle text-[32px] text-[#d9cac5]">
+              I am a FullStack developer
             </p>
             <div className="main-actions flex gap-x-6">
-              <Link to="/works" className="main-body__btn transition-colors w-36 text-center bg-transparent hover:bg-[#01908c] text-white font-semibold hover:text-white py-2 px-4 border border-white hover:border-transparent rounded">
+              <Link
+                to="/works"
+                className="main-body__btn transition-colors  text-[18px] text-center bg-transparent hover:bg-[#01908c] text-white font-semibold hover:text-white py-3 px-7 border border-white hover:border-transparent rounded"
+              >
                 See my works
               </Link>
-              <Link to="/contact" className="main-body__btn transition-all w-36 text-center hover:bg-transparent bg-[#01908c] text-white font-semibold hover:text-white py-2 px-4 hover:border hover:border-white hover:border-transparent rounded">
+              <Link
+                to="/contact"
+                className="main-body__btn transition-all text-[18px] text-center hover:bg-transparent bg-[#01908c] text-white font-semibold hover:text-white py-3 px-8 hover:border hover:border-white hover:border-transparent rounded"
+              >
                 Say hello🖐🏻
               </Link>
             </div>
